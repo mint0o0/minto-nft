fd822c1f5c991e29dce9b3ddc9d82d7a76c0f132
Lee JI HAK <jooin2000@naver.com>
2024-02-14 22:21:57 +0900

    init

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,6 @@
+const Migrations = artifacts.require("Migrations");
+const MyNFT = artifacts.require("MyNFT");
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(MyNFT);
+};
